6d373c06816e8c89f42056d382044f7d82ef4780
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2026-04-04 20:49:28 +0700

    spring boot

---
+++ src/main/java/com/sieuthithucung/SieuthithucungApplication.java
@@ -0,0 +1,13 @@
+package com.sieuthithucung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieuthithucungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SieuthithucungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sieuthithucung/SieuthithucungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sieuthithucung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SieuthithucungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
